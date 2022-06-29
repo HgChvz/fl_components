cf7711f754dda3b841a98b72efb8830d25e3aa40
Hugo Chavez <hgchvz.dev@gmail.com>
2022-06-29 18:23:54 -0400

    Se agregaron rutas a la App

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import 'package:fl_components/screens/screens.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Componentes en Flutter'),
+          elevation: 0,
+        ),
+        body: ListView.separated(
+          itemBuilder: (context, index) => ListTile(
+            title: const Text('Nombre de ruta'),
+            leading: const Icon(Icons.access_time_outlined),
+            onTap: () {
+              /*final route = MaterialPageRoute(
+                  builder: (context) => const Listview1Screen());
+              Navigator.push(context, route);*/
+              Navigator.pushNamed(context, 'card');
+            },
+          ),
+          separatorBuilder: (_, __) => const Divider(),
+          itemCount: 10,
+        ));
+  }
+}

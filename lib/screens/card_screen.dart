723967a7e35469f9baede6fcf37ec36047ff805f
Hugo Chavez <hgchvz.dev@gmail.com>
2023-01-09 18:16:17 -0400

    Agregando el widget Card

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fl_components/themes/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class CardScreen extends StatelessWidget {
@@ -6,10 +7,27 @@ class CardScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('CardScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Card Widget'),
       ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        children: <Widget>[
+          Card(
+            child: Column(
+              children: const [
+                ListTile(
+                  leading: Icon(Icons.photo_album_outlined, color: AppTheme.primary,),
+                  title: Text('Soy un titulo de la tarjeta'),
+                  subtitle: Text('Excepteur ea aliquip labore velit esse velit ea commodo labore fugiat culpa. Officia velit qui commodo adipisicing commodo ea cillum nisi irure. Exercitation voluptate irure officia ad aute ex.'),
+                ),
+              ],
+            ),
+          ),
+
+        ],
+      )
     );
   }
 }
